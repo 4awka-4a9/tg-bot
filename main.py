d280b683e1b2ef03b2c2b9543230c597d69f9d7e
Ян Неверовский <anneverovskij@MacBook-Pro-An.local>
2025-02-19 18:06:43 +0100

    добавлена генерация легкого и сложного пароля, кнопки выбора пароля и исправлены навеное все баги

--- main.py
+++ main.py
@@ -2,43 +2,118 @@ import asyncio
 import logging
 import sys
 import config
+import random
 from os import getenv
 
 from aiogram import Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
+from aiogram import types
 from aiogram.types import Message
+from aiogram import F
+
 
 TOKEN = config.BOT_TOKEN
-print(TOKEN)
-
 
 dp = Dispatcher()
 
+chars = [
+
+    charSmalLetters := [],
+    charBigLetters := [],
+    charNumbers := ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
+    charSpecs := ["!", "@", "$", "%", "#", "&", "*"],
+
+]
+
+for i in "qwertyuiopasdfghjklzxcvbnm":
+
+    charSmalLetters.append(i)
+
+for i in "QWERTYUIOPASDFGHJKLZXCVBNM":
+
+    charBigLetters.append(i)
+
+
+def PasswordGenerate(difficulty):
+
+    endPassword = ""
+    password = ""
+
+    if difficulty == "easy":
+
+        for i in range(random.randrange(3, 4)):
+            password += random.choice(chars[0])
+        for i in range(random.randrange(3, 4)):
+            password += random.choice(chars[1])
+        for i in range(random.randrange(3, 4)):
+            password += random.choice(chars[2])
+
+    else:
+
+        for i in range(random.randrange(3, 4)):
+            password += random.choice(chars[0])
+        for i in range(random.randrange(3, 4)):
+            password += random.choice(chars[1])
+        for i in range(random.randrange(3, 4)):
+            password += random.choice(chars[2])
+        for i in range(random.randrange(3, 4)):
+            password += random.choice(chars[3])
+
+    password = list(password)
+    random.shuffle(password)
+
+    for i in password:
+        endPassword += i
+
+    return(endPassword)
+
 
 @dp.message(CommandStart())
-async def command_start_handler(message: Message) -> None:
+async def cmd_start(message: types.Message):
+    kb = [
+        [types.KeyboardButton(text="Легкий")],
+        [types.KeyboardButton(text="Сложный")]
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+    keyboard=kb,
+    resize_keyboard=True,
+    input_field_placeholder="Каокй пароль сгенерировать?")
 
-    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
 
+    await message.answer("Какой пароль создать?", reply_markup=keyboard)
+
+
+@dp.message(F.text.lower() == "легкий")
+async def easy(message: types.Message):
+    password = PasswordGenerate("easy")
+    await message.answer("Пароль: <tg-spoiler>" + password + "</tg-spoiler>", parse_mode="HTML")
+
+@dp.message(F.text.lower() == "сложный")
+async def hard(message: types.Message):
+    password = PasswordGenerate("hard")
+    await message.answer("Пароль: <tg-spoiler>" + password + "</tg-spoiler>", parse_mode="HTML")
 
 @dp.message()
 async def echo_handler(message: Message) -> None:
+    kb = [
+        [types.KeyboardButton(text="Легкий")],
+        [types.KeyboardButton(text="Сложный")]
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+    keyboard=kb,
+    resize_keyboard=True,
+    input_field_placeholder="Каокй пароль сгенерировать?")
 
-    try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
+
+    await message.answer("Какой пароль создать?", reply_markup=keyboard)
 
 
 async def main() -> None:
-    # Initialize Bot instance with default bot properties which will be passed to all API calls
+
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
-    # And the run events dispatching
     await dp.start_polling(bot)
 
 
